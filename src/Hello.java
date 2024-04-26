ceb50d880ba85d45a5e0fafd156d1dd53c00e54d
Praneetha Chebolu <59798116+praneethacl@users.noreply.github.com>
2024-04-26 18:37:13 +0530

    Update and rename Hello.java to HelloWorld.java

--- src/Hello.java
+++
@@ -1,8 +0,0 @@
-public class Hello {
-    public static void main(String[] args) {
-        new Hello().foo();
-    }
-    public void foo() {
-        System.out.println("Hello, world!");
-    }
-}
---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        new HelloWorld().foo();
+    }
+    public void foo() {
+        System.out.println("Hello, world! Hello, Praneetha!");
+    }
+}
